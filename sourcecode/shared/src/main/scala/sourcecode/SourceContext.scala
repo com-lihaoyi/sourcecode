4651c2fe25e46eb027640df16410d586d74ea31f
Li Haoyi <haoyi.sg@gmail.com>
2018-12-30 12:00:39 +0800

    .

--- sourcecode/shared/src/main/scala/sourcecode/SourceContext.scala
+++ sourcecode/shared/src/main/scala/sourcecode/SourceContext.scala
@@ -1,40 +1,40 @@
 package sourcecode
 
 
-abstract class SourceValue[T]{
+trait SourceValue[T] extends Any{
   def value: T
 }
 abstract class SourceCompanion[T, V <: SourceValue[T]](build: T => V){
   def apply()(implicit s: V): T = s.value
   implicit def wrap(s: T): V = build(s)
 }
-case class Name(value: String) extends SourceValue[String]
+case class Name(value: String) extends AnyVal with SourceValue[String]
 object Name extends SourceCompanion[String, Name](new Name(_)) with NameMacros {
-  case class Machine(value: String) extends SourceValue[String]
+  case class Machine(value: String) extends AnyVal with SourceValue[String]
   object Machine extends SourceCompanion[String, Machine](new Machine(_)) with NameMachineMacros
 }
-case class FullName(value: String) extends SourceValue[String]
+case class FullName(value: String) extends AnyVal with SourceValue[String]
 object FullName extends SourceCompanion[String, FullName](new FullName(_)) with FullNameMacros {
-  case class Machine(value: String) extends SourceValue[String]
+  case class Machine(value: String) extends AnyVal with SourceValue[String]
   object Machine extends SourceCompanion[String, Machine](new Machine(_)) with FullNameMachineMacros
 }
-case class File(value: String) extends SourceValue[String]
+case class File(value: String) extends AnyVal with SourceValue[String]
 object File extends SourceCompanion[String, File](new File(_)) with FileMacros
-case class Line(value: Int) extends SourceValue[Int]
+case class Line(value: Int) extends AnyVal with SourceValue[Int]
 object Line extends SourceCompanion[Int, Line](new Line(_)) with LineMacros
-case class Enclosing(value: String) extends SourceValue[String]
+case class Enclosing(value: String) extends AnyVal with SourceValue[String]
 
 object Enclosing extends SourceCompanion[String, Enclosing](new Enclosing(_)) with EnclosingMacros {
-  case class Machine(value: String) extends SourceValue[String]
+  case class Machine(value: String) extends AnyVal with SourceValue[String]
   object Machine extends SourceCompanion[String, Machine](new Machine(_)) with EnclosingMachineMacros
 }
 
 
-case class Pkg(value: String) extends SourceValue[String]
+case class Pkg(value: String) extends AnyVal with SourceValue[String]
 object Pkg extends SourceCompanion[String, Pkg](new Pkg(_)) with PkgMacros
 
 case class Text[T](value: T, source: String)
 object Text extends TextMacros
 
-case class Args(value: Seq[Seq[Text[_]]]) extends SourceValue[Seq[Seq[Text[_]]]]
+case class Args(value: Seq[Seq[Text[_]]]) extends AnyVal with SourceValue[Seq[Seq[Text[_]]]]
 object Args extends SourceCompanion[Seq[Seq[Text[_]]], Args](new Args(_)) with ArgsMacros
