affced6acf08f02518be9c31aa80a296730a8ba0
Anatolii <anatoliykmetyuk@gmail.com>
2019-11-13 14:19:54 +0100

    Fix TextTests to run on Dotty

--- sourcecode/test/src/sourcecode/TextTests.scala
+++ sourcecode/test/src/sourcecode/TextTests.scala
@@ -5,10 +5,9 @@ object TextTests {
     assert(foo(1) == (1, "1"))
     val bar = Seq("lols")
     assert(foo(bar) == (Seq("lols"), "bar"))
-    // FIXME Don't pass on dotty (second element not ok)
     if (TestUtil.isDotty) {
-      assert(foo(Symbol("lol").toString * 2)._1 == "'lol'lol")
-      assert(foo{println("Hello"); Symbol("lol").toString * 2}._1 == "'lol'lol")
+      assert(foo(Symbol("lol").toString * 2) == ("'lol'lol", "Symbol(\"lol\").toString * 2"))
+      assert(foo{println("Hello"); Symbol("lol").toString * 2} == ("'lol'lol", "Symbol(\"lol\").toString * 2"))
     } else {
       assert(foo(Symbol("lol").toString * 2) == ("'lol'lol", "'lol.toString * 2"))
       assert(foo{println("Hello"); Symbol("lol").toString * 2} == ("'lol'lol", "'lol.toString * 2"))
