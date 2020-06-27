15eaf0144d9cc4c45b2a44735a6366396c5181c7
Guillaume Martres <smarter@ubuntu.com>
2020-06-27 19:20:28 +0200

    Fix running tests on 2.13.3
    Symbol#toString changed, so we can't rely on it in tests.

--- sourcecode/test/src/sourcecode/TextTests.scala
+++ sourcecode/test/src/sourcecode/TextTests.scala
@@ -5,13 +5,8 @@ object TextTests {
     assert(foo(1) == (1, "1"))
     val bar = Seq("lols")
     assert(foo(bar) == (Seq("lols"), "bar"))
-    if (TestUtil.isDotty) {
-      assert(foo(Symbol("lol").toString * 2) == ("'lol'lol", "Symbol(\"lol\").toString * 2"))
-      assert(foo{println("Hello"); Symbol("lol").toString * 2} == ("'lol'lol", "Symbol(\"lol\").toString * 2"))
-    } else {
-      assert(foo(Symbol("lol").toString * 2) == ("'lol'lol", "Symbol(\"lol\").toString * 2"))
-      assert(foo{println("Hello"); Symbol("lol").toString * 2} == ("'lol'lol", "Symbol(\"lol\").toString * 2"))
-    }
+    assert(foo(List("lol").toString * 2) == ("List(lol)List(lol)", "List(\"lol\").toString * 2"))
+    assert(foo{println("Hello"); List("lol").toString * 2} == ("List(lol)List(lol)", "List(\"lol\").toString * 2"))
   }
   def foo[T](v: sourcecode.Text[T]) = (v.value, v.source)
 }
